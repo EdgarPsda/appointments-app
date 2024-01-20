95bf54fef238d5289e702e269699d954956e3d7b
EdgarPsda <edgar.psda@gmail.com>
2024-01-19 22:40:26 -0800

    Create patient
    Added create patient functionality and reset form.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react"
 import Form from "./components/Form"
 import Header from "./components/Header"
 import PatientList from "./components/PatientList"
 
 function App() {
+
+  const [patients, setPatients] = useState([]);
+
   return (
     <div className="container mx-auto mt-20">
       <Header></Header>
       <div className="mt-12 md:flex">
 
-        <Form></Form>
+        <Form
+          patients={patients}
+          setPatients={setPatients}
+        ></Form>
         <PatientList></PatientList>
 
       </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,50 @@
 import { useState, useEffect } from "react";
 
-function Form(){
+function Form({patients, setPatients }){
 
     const [petName, setPetName] = useState('');
+    const [owner, setOwner] = useState('');
+    const [email, setEmail] = useState('');
+    const [medRelease, setMedRelease] = useState('');
+    const [symptoms, setSymtoms] = useState('');
+
+    const [error, setError] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if([petName, owner, email, medRelease, symptoms].includes('')){
+            console.log("Hay al menos un campo vacio");
+            setError(true);
+        }else{
+            console.log("Todos llenos");
+            setError(false);
+
+            // Patient object
+            const patientObj = {
+                petName,
+                owner,
+                email,
+                medRelease,
+                symptoms
+            }
+
+            setPatients([...patients, patientObj]);
+
+            // Reset form
+            setPetName('');
+            setOwner('');
+            setEmail('');
+            setMedRelease('');
+            setSymtoms('');
+        }
+
+        console.log("Enviando formulario")
+    }
 
 
     return (
-        <div className="md:w-1/2 lg:w-2/5">
+        <div className="md:w-1/2 lg:w-2/5 mx-5">
             <h2 className="font-black text-3xl text-center">Patient Follow-up</h2>
             <p className="text-lg mt-5 text-center mb-10">
                 Add Patient and {""}
@@ -14,7 +52,16 @@ function Form(){
             </p>
 
 
-            <form action="" className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+            <form 
+                action="" 
+                className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+                onSubmit={handleSubmit}     
+            >
+                {error && (
+                    <div className="bg-red-700 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
+                        <p>All fields are required</p>
+                    </div>
+                )}
                 <div className="mb-5">
                     <label className="block text-gray-700 uppercase font-bold" htmlFor="petname">Pet Name</label>
                     <input 
@@ -34,6 +81,8 @@ function Form(){
                         type="text" 
                         placeholder="Owner"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={owner}
+                        onChange={(e) => setOwner(e.target.value)}
                     />
                 </div>
 
@@ -44,6 +93,8 @@ function Form(){
                         type="email" 
                         placeholder="Owner email"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                 </div>
 
@@ -53,6 +104,8 @@ function Form(){
                         id="medrelease"
                         type="date" 
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={medRelease}
+                        onChange={(e) => setMedRelease(e.target.value)}
                     />
                 </div>
 
@@ -63,6 +116,8 @@ function Form(){
                         id="symptoms" 
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400"
                         placeholder="Describe the symptoms"
+                        value={symptoms}
+                        onChange={(e) => setSymtoms(e.target.value)}
                         
                     />
                 </div>
